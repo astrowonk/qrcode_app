da271fc6408fb38989901f647cd175a321210cd4
Marcos Huerta <marcos@marcoshuerta.com>
2023-09-03 12:08:03 -0400

    add image type optoins

--- dash_app.py
+++ dash_app.py
@@ -7,6 +7,20 @@ app = Dash(__name__, external_stylesheets=[dbc.themes.YETI])
 app.layout = html.Div([
     html.H1(children='QR Code App', style={'textAlign': 'center'}),
     dbc.InputGroup([
+        dbc.InputGroupText("Image Type and Scale"),
+        dbc.Input(id='scale', name="Scale", type='number', value=10),
+        dbc.Select(id='img-type',
+                   options=['svg', 'png'],
+                   name="Image Type",
+                   persistence=True,
+                   persistence_type='local',
+                   value='svg'),
+    ],
+                   style={
+                       "width": "60%",
+                       'margin': 'auto'
+                   }),
+    dbc.InputGroup([
         dbc.Input(
             id='content',
             value='',
@@ -34,15 +48,19 @@ app.layout = html.Div([
 @callback(
     Output('output-image', 'src'),
     State('content', 'value'),
+    State('img-type', 'value'),
+    State('scale', 'value'),
     Input('go', 'n_clicks'),
 )
-def update_code(content, _):
+def update_code(content, img_type, scale, _):
     if not content:
         return ''
 
     _qrcode = segno.make(content, micro=False)
-
-    return _qrcode.svg_data_uri()
+    print(type(scale))
+    if img_type == "svg":
+        return _qrcode.svg_data_uri(scale=scale)
+    return _qrcode.png_data_uri(scale=scale)
 
 
 if __name__ == '__main__':
