0e8ace7fd246b634effc4800092488bc7deadd12
Marcos Huerta <marcos@marcoshuerta.com>
2025-01-20 06:41:58 -0500

    add error level

--- dash_app.py
+++ dash_app.py
@@ -6,50 +6,67 @@ from pathlib import Path
 
 parent_dir = Path().absolute().stem
 
-app = Dash(__name__,
-           url_base_pathname=f"/dash/{parent_dir}/",
-           external_stylesheets=[dbc.themes.YETI],
-           title="QR Code Generator",
-           meta_tags=[
-               {
-                   "name": "viewport",
-                   "content": "width=device-width, initial-scale=1"
-               },
-           ])
-app.layout = html.Div([
-    html.H1(children='QR Code App', style={'textAlign': 'center'}),
-    dbc.InputGroup([
-        dbc.InputGroupText("Image Type and Scale"),
-        dbc.Input(id='scale', name="Scale", type='number', value=10),
-        dbc.Select(id='img-type',
-                   options=['svg', 'png'],
-                   name="Image Type",
-                   persistence=True,
-                   persistence_type='local',
-                   value='svg'),
+app = Dash(
+    __name__,
+    url_base_pathname=f'/dash/{parent_dir}/',
+    external_stylesheets=[dbc.themes.YETI],
+    title='QR Code Generator',
+    meta_tags=[
+        {'name': 'viewport', 'content': 'width=device-width, initial-scale=1'},
     ],
-                   style={
-                       "width": "60%",
-                       'margin': 'auto'
-                   }),
-    dbc.InputGroup([
-        dbc.Input(
-            id='content',
-            value='',
-            debounce=True,
-            placeholder="Enter text for QR Code",
+)
+app.layout = html.Div(
+    [
+        html.H1(children='QR Code App', style={'textAlign': 'center'}),
+        dbc.InputGroup(
+            [
+                dbc.InputGroupText('Image Type and Scale'),
+                dbc.Input(id='scale', name='Scale', type='number', value=10),
+                dbc.Select(
+                    id='img-type',
+                    options=['svg', 'png'],
+                    name='Image Type',
+                    persistence=True,
+                    persistence_type='local',
+                    value='svg',
+                ),
+            ],
+            style={'width': '60%', 'margin': 'auto'},
         ),
-        dbc.Button(id='go', children="Make QR Code!"),
-    ],
-                   style={
-                       "width": "60%",
-                       "margin": "auto"
-                   }),
-    html.Div(id='my-output', style={
-        'text-align': 'center',
-        'margin': 'auto'
-    })
-])
+        dbc.InputGroup(
+            [
+                dbc.InputGroupText('Error Correction'),
+                dbc.Select(
+                    id='error-level',
+                    options=[
+                        {'value': 'L', 'label': 'L (Min)'},
+                        {'value': 'M', 'label': 'M'},
+                        {'value': 'Q', 'label': 'Q'},
+                        {'value': 'H', 'label': 'H (Max)'},
+                    ],
+                    name='Error Level',
+                    persistence=True,
+                    persistence_type='local',
+                    value='L',
+                ),
+            ],
+            style={'width': '60%', 'margin': 'auto'},
+        ),
+        dbc.InputGroup(
+            [
+                dbc.Input(
+                    id='content',
+                    value='',
+                    debounce=True,
+                    placeholder='Enter text for QR Code',
+                ),
+                dbc.Button(id='go', children='Make QR Code!'),
+            ],
+            style={'width': '60%', 'margin': 'auto'},
+        ),
+        html.Div(id='my-output', style={'text-align': 'center', 'margin': 'auto'}),
+    ]
+)
 
 
 @callback(
@@ -57,14 +74,15 @@ app.layout = html.Div([
     Input('content', 'value'),
     Input('img-type', 'value'),
     Input('scale', 'value'),
+    Input('error-level', 'value'),
     Input('go', 'n_clicks'),
 )
-def update_code(content, img_type, scale, _):
+def update_code(content, img_type, scale, error, _):
     if not content:
         return ''
 
-    _qrcode = segno.make(content, micro=False)
-    if img_type == "svg":
+    _qrcode = segno.make_qr(content, error=error)
+    if img_type == 'svg':
         _img = _qrcode.svg_data_uri(scale=scale)
     else:
         _img = _qrcode.png_data_uri(scale=scale)
@@ -74,20 +92,16 @@ def update_code(content, img_type, scale, _):
             id='output-image',
             src=_img,
             width=400,
-            style={
-                'margin': 'auto',
-                'display': 'block'
-            },
+            style={'margin': 'auto', 'display': 'block'},
         ),
-        html.A(download=f'qrcode.{img_type}',
-               href=_img,
-               role='button',
-               children=f"Download {img_type.upper()}",
-               className="btn btn-primary",
-               style={
-                   'text-align': 'center',
-                   'margin': 'auto'
-               })
+        html.A(
+            download=f'qrcode.{img_type}',
+            href=_img,
+            role='button',
+            children=f'Download {img_type.upper()}',
+            className='btn btn-primary',
+            style={'text-align': 'center', 'margin': 'auto'},
+        ),
     ]
 
 
